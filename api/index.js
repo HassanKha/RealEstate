70c8ca232ccddf38c56c116dbc9a8bb71ad74b15
Hassan Khaled <58123330+HassanKha@users.noreply.github.com>
2023-12-27 23:16:21 +0200

    last update

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO)
   });
 
 const app = express();
-const _dirname = path.resolve();
+const __dirname = path.resolve();
 app.use(express.json())
 
 app.use(cookieParser())
